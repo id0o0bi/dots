fd9c6d148d3a34a0cf08b11118fc103e8856dc3b
id0o0bi <id0o0bi.org@gmail.com>
2026-05-08 16:53:47 +0800

    wttr: silence curl output with -s flag

--- ags/services/vendors/WttrIn.ts
+++ ags/services/vendors/WttrIn.ts
@@ -4,7 +4,7 @@ import { WeatherData } from "../type";
 import { shAsync } from "../util";
 import { setWeather } from "../../widgets/Components/Weather";
 
-const CMD = `curl -m 10 wttr.in/?format=j1 > ${WTTR_CACHE} 2>/dev/null`;
+const CMD = `curl -s -m 10 wttr.in/?format=j1 > ${WTTR_CACHE} 2>/dev/null`;
 
 export function getFromWttrIn(): void {
   try {
